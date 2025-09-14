af419e5bb15e20c1425bcf6a73341209bcc31e0d
serhii.mazurenko <serhii.mazurenko@nixsolutions.com>
2025-09-14 20:46:21 +0200

    fix unit tests

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+  },
+};
+
+export default createJestConfig(customJestConfig);
