504407cce52ed83285d4056ca22ba2eb7c7237a1
serhii.mazurenko <serhii.mazurenko@nixsolutions.com>
2025-09-15 17:25:51 +0200

    final fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import { HealthStatus } from "@/components/HealthStatus";
-import HealthStatusServer from "@/components/HealthStatusServer";
-import JsonStatusServer from "@/components/JsonStatusServer";
+//import { HealthStatus } from "@/components/HealthStatus";
+//import HealthStatusServer from "@/components/HealthStatusServer";
+//import JsonStatusServer from "@/components/JsonStatusServer";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
